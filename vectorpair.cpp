f312621c3aedbf59caac3bbe34eb31fc721ec17b
veera-durga-jyothi <69592088+veera-durga-jyothi@users.noreply.github.com>
2022-04-07 11:47:59 +0530

    Add files via upload

---
+++ vectorpair.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<pair<int,int>>vect={{1,2},{3,4},{4,5}};
+     for(auto it:vect)
+     {
+	    cout<<it.first<<" "<<it.second<<"\n";
+     }
+ }
